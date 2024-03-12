9505931aa2f80ea81747bd6fdd6e3bf5976cb249
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2024-03-12 19:09:05 +0100

    glfw3 works

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
@@ -8,5 +10,34 @@ int main() {
     glm::vec3 sum = vector1 + vector2;
     std::cout << "Sum: " << sum.x << ", " << sum.y << ", " << sum.z << std::endl;
 
-    return 0;
+    GLFWwindow* window;
+
+    if (!glfwInit()){
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Failed to create window" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
