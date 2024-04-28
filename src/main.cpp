b8847ae2c44b04172991124678cdecd61d36f48e
taylor-swif <57270771+taylor-swif@users.noreply.github.com>
2024-04-28 22:20:25 +0200

    cleaning main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-// Std. Includes
-#include <string>
-
 // GLEW
 #include <GL/glew.h>
 
@@ -11,17 +8,8 @@
 #include "Shader.h"
 #include "Camera.h"
 
-// GLM Mathemtics
-#include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
-// Other Libs
-#include "SOIL2/SOIL2.h"
-
-#include "Texture.h"
-#include "VertexBuffer.h"
-#include "VertexArray.h"
 #include "Window.h"
 #include "Renderer.h"
 
@@ -75,86 +63,7 @@ int main( )
     // enable alpha support
     glEnable( GL_BLEND );
     glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
-    
-    // Setup and compile our shaders
-    Shader ourShader( "/Users/Kamil/Documents/Studia/pomidor/src/res/shaders/core.vs", "/Users/Kamil/Documents/Studia/pomidor/src/res/shaders/core.frag" );
-    // Set up our vertex data (and buffer(s)) and attribute pointers
-    GLfloat vertices[] =
-    {
-            //front
-            // x     y     z     u     v
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-        //back
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        //left
-        -0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
 
-        // right
-        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-        0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        // bottom
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        // top
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-    };
-    
-    glm::vec3 cubePositions[] =
-    {
-        glm::vec3( 0.0f, 0.0f, 0.0f ),
-        glm::vec3( 2.0f, 5.0f, -15.0f ),
-        glm::vec3( -1.5f, -2.2f, -2.5f ),
-        glm::vec3( -3.8f, -2.0f, -12.3f ),
-        glm::vec3( 2.4f, -0.4f, -3.5f ),
-        glm::vec3( -1.7f, 3.0f, -7.5f ),
-        glm::vec3( 1.3f, -2.0f, -2.5f ),
-        glm::vec3( 1.5f, 2.0f, -2.5f ),
-        glm::vec3( 1.5f, 0.2f, -1.5f ),
-        glm::vec3( -1.3f, 1.0f, -1.5f )
-    };
-
-
-    VertexArray va;
-    VertexBuffer vb(vertices, sizeof(vertices));
-
-    va.Bind();
-    vb.Bind();
-
-    va.AddBuffer(vb, 0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
-    va.AddBuffer(vb, 2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-
-    va.Unbind();
-    vb.Unbind();
-
-    Texture texture("/Users/Kamil/Documents/Studia/pomidor/src/res/images/image1.jpg");
 
     Renderer renderer("/Users/Kamil/Documents/Studia/pomidor/src/res/shaders/core.vs", "/Users/Kamil/Documents/Studia/pomidor/src/res/shaders/core.frag", "/Users/Kamil/Documents/Studia/pomidor/src/res/images/image1.jpg");
     World world;
@@ -170,40 +79,6 @@ int main( )
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-//        // Activate the shader before setting uniforms
-//        ourShader.Use();
-//
-//        // Bind Texture using the texture class
-//        texture.Bind(0);  // Bind to texture unit 0
-//        glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture1"), 0);
-//
-//        glm::mat4 projection = glm::perspective(camera.GetZoom(), (GLfloat)SCREEN_WIDTH / (GLfloat)SCREEN_HEIGHT, 0.1f, 1000.0f);
-//        glm::mat4 view = camera.GetViewMatrix();
-//
-//        GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
-//        GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
-//        GLint projLoc = glGetUniformLocation(ourShader.Program, "projection");
-//
-//        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-//        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-//
-//        // Bind VAO
-//        va.Bind();
-//        for (GLuint i = 0; i < 10; i++) {
-//            glm::mat4 model = glm::mat4(1.0f);
-//            model = glm::translate(model, cubePositions[i]);
-//            GLfloat angle = 20.0f * 0;  // This seems like it was meant to be dynamic
-//            model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
-//
-//            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-//            glDrawArrays(GL_TRIANGLES, 0, 36);
-//        }
-//
-//        // Unbind VAO
-//        va.Unbind();
-//
-//        // Unbind texture when done, to prevent accidentally using this texture elsewhere
-//        texture.Unbind();
         renderer.Render(world, camera);
 
         // Swap buffers
