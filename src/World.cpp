ea473d12b009b86c2d3363dc6e3856217dc3e4b5
farodoc <panjakubkonopka@gmail.com>
2024-05-02 18:50:11 +0200

    Improved world generation with different biomes

--- src/World.cpp
+++ src/World.cpp
@@ -2,12 +2,12 @@
 #include <ctime>
 #include <iostream>
 
-std::vector<std::vector<float>> World::GeneratePerlinNoise(int width, int height, int octave) {
+std::vector<std::vector<float>> World::GenerateHeightMap(int width, int height, int octave) {
     std::vector<std::vector<float>> noiseValues(width, std::vector<float>(height, 0.0f));
 
     int centerX = width / 2;
     int centerY = height / 2;
-    float frequency = 0.012;
+    float frequency = 0.015;
 
     for (int x = 0; x < width; ++x) {
         for (int y = 0; y < height; ++y) {
@@ -32,9 +32,30 @@ std::vector<std::vector<float>> World::GeneratePerlinNoise(int width, int height
     return noiseValues;
 }
 
+std::vector<std::vector<float>> World::GenerateBiomeMap(int width, int height, int octave) {
+    std::vector<std::vector<float>> biomeValues(width, std::vector<float>(height, 0.0f));
+    float frequency = 0.005;
+
+    for (int x = 0; x < width; ++x) {
+        for (int y = 0; y < height; ++y) {
+            float noise = static_cast<float>(perlin.octave2D_01((x * frequency), (y * frequency), octave));
+            biomeValues[x][y] = noise;
+        }
+    }
+
+    return biomeValues;
+}
+
 //World::World() : perlin(123456u) {
 World::World() : perlin(static_cast<unsigned int>(std::time(nullptr))) {
-    std::vector<std::vector<float>> noiseValues = GeneratePerlinNoise(NOISE_WIDTH, NOISE_HEIGHT, NOISE_OCTAVE);
+    std::vector<std::vector<float>> heightMap = GenerateHeightMap(NOISE_WIDTH, NOISE_HEIGHT, NOISE_OCTAVE);
+    std::vector<std::vector<float>> biomeMap = GenerateBiomeMap(NOISE_WIDTH, NOISE_HEIGHT, BIOME_OCTAVE);
+
+    for(int x = 0; x < CHUNK_SIZE; x++){
+        for(int z = 0; z < CHUNK_SIZE; z++){
+            biomeMap[x][z] *= heightMap[x][z];
+        }
+    }
 
     for (int i = 0; i < WORLD_SIZE; ++i) {
         for (int j = 0; j < WORLD_SIZE; ++j) {
@@ -48,16 +69,21 @@ World::World() : perlin(static_cast<unsigned int>(std::time(nullptr))) {
                         int globalZ = j * CHUNK_SIZE + z;
                         Block block;
 
-                        if (y <= noiseValues[globalX][globalZ] * CHUNK_HEIGHT) {
-                            if(y == 0){
-                                block = Block(8);
-                            }
-                            else if(y <= 3){
+                        if (y <= heightMap[globalX][globalZ] * CHUNK_HEIGHT) {
+                            if(y <= WATER_LEVEL + 3){
                                 block = Block(7);
                             }
                             else{
-                                block = Block(0);
+                                if(biomeMap[globalX][globalZ] < 0.3){
+                                    block = Block(7);
+                                } else if(biomeMap[globalX][globalZ] < 0.55){
+                                    block = Block(0);
+                                } else{
+                                    block = Block(3);
+                                }
                             }
+                        } else if(y <= WATER_LEVEL){
+                            block = Block(8);
                         } else {
                             block = Block(0, false);
                         }
--- src/World.h
+++ src/World.h
@@ -25,11 +25,14 @@ private:
     static constexpr int CHUNK_SIZE = Chunk::CHUNK_SIZE;
     static constexpr int CHUNK_HEIGHT = Chunk::CHUNK_HEIGHT;
     static constexpr int WORLD_SIZE = 20;
-    static constexpr int NOISE_OCTAVE = 12;
+    static constexpr int NOISE_OCTAVE = 4;
+    static constexpr int BIOME_OCTAVE = 12;
     static constexpr int NOISE_WIDTH = CHUNK_SIZE * WORLD_SIZE;
     static constexpr int NOISE_HEIGHT = CHUNK_SIZE * WORLD_SIZE;
+    static constexpr int WATER_LEVEL = CHUNK_HEIGHT / 10;
 
-    std::vector<std::vector<float>> GeneratePerlinNoise(int width, int height, int octave);
+    std::vector<std::vector<float>> GenerateHeightMap(int width, int height, int octave);
+    std::vector<std::vector<float>> GenerateBiomeMap(int width, int height, int octave);
 
     std::map<std::pair<int, int>, Chunk> chunks;
 
